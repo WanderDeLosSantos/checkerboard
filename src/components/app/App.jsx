31022009fe3cb3f2f94c5d16d42d76045c778c9f
Wander De Los Santos <wdd16@scarletmail.rutgers.edu>
2020-05-13 15:53:30 -0400

    initial commit, create-react-app boilerplate

---
+++ src/components/app/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+// components
+import { Board } from 'components'
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
